860d3f27e1f3e12ed178aa06adcb0557b7e7dbe1
Christodoulopoulos, Christos <chrchrs@amazon.co.uk>
2019-03-28 09:41:05 +0000

    Added ability to print the raw attention matrix

--- single_image_inference.py
+++ single_image_inference.py
@@ -52,7 +52,7 @@ def run(question, img):
     att_upsampled = skimage.transform.pyramid_expand(att_matrix.data.cpu().numpy(), upscale=32)
     att_greyscale = numpy.array(255*(att_upsampled - numpy.min(att_upsampled))/numpy.ptp(att_upsampled), numpy.uint8)
     _, pred_index = out.max(dim=1, keepdim=True)
-    return index_to_answer[pred_index.data[0][0]], att_greyscale
+    return index_to_answer[pred_index.data[0][0].item()], att_matrix, att_greyscale
 
 
 def apply_attention_to_image(cropped_img, activation_map):
@@ -62,6 +62,25 @@ def apply_attention_to_image(cropped_img, activation_map):
     img_with_heatmap = numpy.float32(activation_heatmap) + numpy.float32(cropped_img_trans)
     img_with_heatmap = img_with_heatmap / numpy.max(img_with_heatmap)
     return numpy.uint8(255 * img_with_heatmap)
+
+
+def single_image_predict(image_path, question):
+    pil_image = Image.open(image_path).convert('RGB')
+    cropped_image = transform(pil_image)
+    answer, attention, attention_scaled = run(question, cropped_image)
+    return cropped_image, answer, attention, attention_scaled
+
+
+def test():
+    # Setup the image for inference
+    image_path = '/home/chrchrs/vqa-data/train2014/COCO_train2014_000000144646.jpg'
+    cropped_image, answer, raw_att, attention = single_image_predict(image_path, 'How many bikes are there?')
+    print(answer)
+    cv2.imwrite(image_path + '_hm.jpg', apply_attention_to_image(cropped_image, attention))
+
+    cropped_image, answer, raw_att, attention = single_image_predict(image_path, 'What color are the jackets?')
+    print(answer)
+    cv2.imwrite(image_path + '_hm2.jpg', apply_attention_to_image(cropped_image, attention))
 
 
 if __name__ == '__main__':
@@ -85,15 +104,16 @@ if __name__ == '__main__':
     index_to_answer = {answer_to_index[key]: key for key in answer_to_index.keys()}
     max_question_length = 23
 
-    # Setup the image for inference
-    image_path = '/home/chrchrs/vqa-data/train2014/COCO_train2014_000000144646.jpg'
-    pil_image = Image.open(image_path).convert('RGB')
-    cropped_image = transform(pil_image)
+    home_dir = '/home/chrchrs/vqa-data/'
+    image_dir = home_dir + 'val2014/'
+    data_file = home_dir + 'joshua_chosenQns_22Mar.tsv'
+    out_file = home_dir + 'joshua_chosenQns_22Mar_att.json'
+    attentions = {}
 
-    answer, attention = run('How many bikes are there?', cropped_image)
-    print(answer)
-    cv2.imwrite(image_path + '_hm.jpg', apply_attention_to_image(cropped_image, attention))
-
-    answer, attention = run('What color are the jackets?', cropped_image)
-    print(answer)
-    cv2.imwrite(image_path + '_hm2.jpg', apply_attention_to_image(cropped_image, attention))
+    for line in open(data_file).readlines():
+        image = line.split('\t')[0]
+        question = line.split('\t')[1]
+        cropped_image, answer, att14x14, attention_scaled = single_image_predict(image_dir+image, question)
+        att_matrix = att14x14.data.cpu().numpy().tolist()
+        attentions[image+'#'+question] = att_matrix
+    json.dump(attentions, open(out_file, 'w'))
